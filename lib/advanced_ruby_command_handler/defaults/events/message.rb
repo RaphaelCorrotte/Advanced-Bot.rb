1a49f88cbc159539b099a3665c6fd417bcce1a0a
senchuu <senchuuuu@gmail.com>
2021-07-25 11:01:03 +0200

    Create defaults events

---
+++ lib/advanced_ruby_command_handler/defaults/events/message.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative "../../app/utils"
+
+module Events
+  def self.message(client)
+    client.message(:start_with => client.config[:prefix]) do |message|
+      args = message.content.slice(client.config[:prefix].size, message.content.size).split(" ")
+      name = args.shift
+      command = CommandHandler::Utils.get_command(name)
+
+      next unless command
+
+      command.run.call(message, client)
+    end
+  end
+end
