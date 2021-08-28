0011e616857b63d8239741979e79da37f2950c67
senchuu <senchuuuu@gmail.com>
2021-08-28 11:06:49 +0200

    Updating main file

--- lib/advanced_ruby_command_handler.rb
+++ lib/advanced_ruby_command_handler.rb
@@ -4,7 +4,6 @@ require_relative "advanced_ruby_command_handler/app/app"
 require_relative "advanced_ruby_command_handler/app/command"
 require_relative "advanced_ruby_command_handler/app/utils"
 
-
 module CommandHandler
   def self.new(commands_dir: "commands", events_dir: "events", config_file: "config.yml")
     AdvancedRubyCommandHandler::Client.new(:commands_dir => commands_dir, :events_dir => events_dir, :config_file => config_file)
